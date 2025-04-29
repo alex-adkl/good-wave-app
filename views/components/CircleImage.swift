05125122cb05c9094b76868d6e90e0d5c80afced
alex <alexadkl@gmail.com>
2025-04-29 16:49:11 +0200

    afficher image depuis URL

--- views/components/CircleImage.swift
+++ views/components/CircleImage.swift
@@ -1,16 +1,27 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var url: URL?
     var body: some View {
-        Image("UXSpv5pRxVI8yxQyAPt28tObygpmSCRMaaxsLnfm2Oc")
-            .resizable()
-            .scaledToFill()
-                .frame(width: 200, height: 200)
-            .clipShape(Circle())
-            
+        AsyncImage(url: url) { phase in
+            switch phase {
+            case .empty:
+                ProgressView() // chargement
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+            case .failure:
+                Image(systemName: "photo")
+            @unknown default:
+                EmptyView()
+            }
+        }
     }
 }
 
 #Preview {
-    CircleImage()
+    CircleImage(url: URL(string: "https://images.unsplash.com/photo-1455264745730-cb3b76250ae8?q=80&w=2233&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"))
 }
